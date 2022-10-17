150c5ff2eecfb8c53016aee80295303b84418cdb
karsh-404~ <utkarshporwal26@gmail.com~>
2022-10-18 00:46:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# music_webapp
+## Music is the best Addiction
+![the real screenshot](https://github.com/Aayush-404/music_webapp/blob/master/ss_web.PNG)
---
+++ lib/screens/playlist_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_spotify_ui/data/data.dart';
+import 'package:flutter_spotify_ui/widgets/widgets.dart';
+
+class PlaylistScreen extends StatefulWidget {
+  final Playlist playlist;
+
+  const PlaylistScreen({
+    Key? key,
+    required this.playlist,
+  }) : super(key: key);
+
+  @override
+  _PlaylistScreenState createState() => _PlaylistScreenState();
+}
+
+class _PlaylistScreenState extends State<PlaylistScreen> {
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(126, 41, 227, 147),
+        elevation: 0,
+        leadingWidth: 140.0,
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                customBorder: const CircleBorder(),
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(6.0),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(188, 184, 227, 53),
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(Icons.chevron_left, size: 28.0),
+                ),
+              ),
+              const SizedBox(width: 16.0),
+              InkWell(
+                customBorder: const CircleBorder(),
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(6.0),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(131, 116, 10, 196),
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(Icons.chevron_right, size: 28.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton.icon(
+            style: TextButton.styleFrom(
+              primary: Theme.of(context).iconTheme.color,
+            ),
+            onPressed: () {},
+            icon: const Icon(
+              Icons.account_circle_outlined,
+              size: 30.0,
+            ),
+            label: const Text('Utkarsh & Aayush'),
+          ),
+          const SizedBox(width: 8.0),
+          IconButton(
+            padding: const EdgeInsets.only(),
+            icon: const Icon(Icons.keyboard_arrow_down, size: 30.0),
+            onPressed: () {},
+          ),
+          const SizedBox(width: 20.0),
+        ],
+      ),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(152, 69, 40, 197),
+              Theme.of(context).backgroundColor,
+            ],
+            stops: const [0, 0.3],
+          ),
+        ),
+        child: Scrollbar(
+          isAlwaysShown: true,
+          controller: _scrollController,
+          child: ListView(
+            controller: _scrollController,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 60.0,
+            ),
+            children: [
+              PlaylistHeader(playlist: widget.playlist),
+              TracksList(tracks: widget.playlist.songs),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
